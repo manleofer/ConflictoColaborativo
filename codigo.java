03f9bfc68a93bab8c24c4ddb747875d18a8ae54a
Samohe89 <smorher483@iesalmudeyne.es>
2024-10-23 09:39:51 +0200

    Agrego linea6

--- codigo.java
+++ codigo.java
@@ -3,5 +3,6 @@ public class codigo {
     public static void main (String [] args) {
         System.out.println("Hola compi!");
         System.out.println("Vamos a putearnos");
+        System.out.println("Quieres guerra, eh?");
     }
 }
