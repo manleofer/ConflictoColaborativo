09d4f5fbaff5d706acce1b52082942010a003f84
manleofer <mleofer304@iesalmudeyne.es>
2024-10-23 09:42:34 +0200

    vuelvo a modificar la linea 6

--- codigo.java
+++ codigo.java
@@ -3,5 +3,6 @@ public class codigo {
     public static void main (String [] args) {
         System.out.println("Hola compi!");
         System.out.println("Vamos a ayudarnos a comprender el ejercicio");
+        System.out.println("Al final quiero la guerra");
     }
 }
