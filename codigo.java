c6606be76bd27b0e9863b25c937af8c1bffc62ac
manleofer <mleofer304@iesalmudeyne.es>
2024-10-23 09:36:50 +0200

    modifico el codigo para que de conflicto la linea 5

--- codigo.java
+++ codigo.java
@@ -2,6 +2,6 @@
 public class codigo {
     public static void main (String [] args) {
         System.out.println("Hola compi!");
-        System.out.println("Vamos a putearnos");
+        System.out.println("Vamos a ayudarnos a comprender el ejercicio");
     }
 }
